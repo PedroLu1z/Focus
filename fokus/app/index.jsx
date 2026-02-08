960ce27fd5e72bc7c6336696899a032a9e127644
PedroLu1z <pedroluizssantana@outlook.com>
2026-02-08 15:22:48 -0300

    Alterado estilos e criado componentes iniciais do app

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Edit app/index.jsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require('./pomodoro.png')} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25.00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto fictício e sem fins comerciais.</Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura</Text>
+      </View>
     </View>
   );
 }
@@ -17,8 +25,39 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#021123",
+    gap: 40
   },
-  text: {
-    color: '#FFF',
+  actions: {
+    padding: 24,
+    backgroundColor: "#14448080",
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480',
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    fontWeight: 'bold',
+    color: "#FFF",
+    textAlign: 'center', 
+  },
+  button: {
+    backgroundColor: "#BB72FF",
+    borderRadius: 32,
+    padding: 8, 
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18,
+  },
+  footer:{
+   width: '80%',
+  },
+  footerText: {
+    textAlign: 'center',    
+    color: '#98A0A8',
+    fontSize: 12.5,
   }
-})
+}) 

3fea0359d0265e450cd14a8fb0a5bd719b64ab9d
PedroLu1z <pedroluizssantana@outlook.com>
2026-02-08 15:57:47 -0300

    Adicionado novos estilos e criado Foco, Pausa Curta e Pausa Longa

--- fokus/app/index.jsx
+++ fokus/app/index.jsx
@@ -5,6 +5,23 @@ export default function Index() {
     <View style={styles.container}>
       <Image source={require('./pomodoro.png')} />
       <View style={styles.actions}>
+        <View style={styles.context}>
+          <Pressable style={styles.contextButtonActive}>
+            <Text style={styles.contextButtonText}>Foco</Text>
+          </Pressable>
+
+          <Pressable> 
+            <Text style={styles.contextButtonText}>
+              Pausa Curta
+            </Text>
+          </Pressable>
+
+          <Pressable>
+            <Text style={styles.contextButtonText}>
+              Pausa Longa
+            </Text>
+          </Pressable>
+        </View>
         <Text style={styles.timer}>25.00</Text>
         <Pressable style={styles.button}>
           <Text style={styles.buttonText}>Começar</Text>
@@ -36,6 +53,20 @@ const styles = StyleSheet.create({
     borderColor: '#144480',
     gap: 32,
   },
+  context:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  contextButtonText: {
+    fontSize: 12.5,
+    color: '#FFF', 
+    padding: 8, 
+  },
+  contextButtonActive: {
+    backgroundColor: '#144480',
+    borderRadius: 8
+  }, 
   timer: {
     fontSize: 54,
     fontWeight: 'bold',
@@ -59,5 +90,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',    
     color: '#98A0A8',
     fontSize: 12.5,
-  }
+  },
+
 }) 
